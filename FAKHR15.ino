8db758d8d78aa75c986449b9780e7849a8bcca90
Fakhrr9950 <166295478+Fakhrr9950@users.noreply.github.com>
2024-05-28 13:32:28 +0330

    Add files via upload

---
+++ FAKHR15.ino
@@ -0,0 +1,21 @@
+ #define BUZZER_PIN 6
+ #define BTN_PIN 7
+ void setup() {
+  // put your setup code here, to run once:
+  pinMode(BUZZER_PIN,OUTPUT);
+  pinMode(BTN_PIN,INPUT_PULLUP);
+
+}
+
+void loop() {
+  if(digitalRead(BTN_PIN))
+  {
+    analogWrite(BUZZER_PIN, 255);
+  }
+  else
+  {
+    analogWrite(BUZZER_PIN, 127);
+  }
+  // put your main code here, to run repeatedly:
+
+}
